11e60c3f7eac22050dbb610b53bd9d01264bcc35
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2023-08-27 18:22:23 +0000

    aligned pointer main.cpp

--- Pointer/Main.cpp
+++ Pointer/Main.cpp
@@ -7,13 +7,12 @@ int main(int argc, char *argv[])
     int a[] = {10, 15, 4, 25, 3, -4};
     int *p = &a[2]; // element #2 == 4
 
-cout << p << "\t" << *p << "\t" << *(p) << endl;
+    cout << p << "\t" << *p << "\t" << *(p) << endl;
 
-    cout << "\na. *(p+1): "<< *(p+1) 
-    <<"\nb. *p[-1]:  "<< p[-1] 
-    <<"\nc. p - a: " <<p - a 
-    <<"\nd. a[*p++]: " << a[*p++] 
-    << "\ne. *(a+a[2]): "<< *(a+a[2]) <<endl;
+    cout << "\na. *(p+1): " << *(p + 1)
+         << "\nb. *p[-1]:  " << p[-1]
+         << "\nc. p - a: " << p - a
+         << "\nd. a[*p++]: " << a[*p++]
+         << "\ne. *(a+a[2]): " << *(a + a[2]) << endl;
     return 0;
 }
-
