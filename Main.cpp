305626c59d421c98733f59bad543dbd0eb9a4a6d
Peter Ramos <dchamp16@gmail.com>
2023-09-12 15:19:50 -0600

    update

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,52 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
+
+union student
+{
+    int roll_no;
+    int phone_number;
+    char name[30];
+};
 
 int main()
 {
-    char *foo[2];
-    char s1[] = "Peter", s2[] = "Justin", s3[] = "Ramos";
+    union student p1;
+    p1.roll_no = 1;
+    p1.phone_number = 1234567822;
+    strcpy(p1.name, "Brown");
+    cout << "roll_no : " << p1.roll_no << endl;
+    cout << "phone_number : " << p1.phone_number << endl;
+    cout << "name : " << p1.name << endl;
 
-    foo[0] = s1;
-    foo[1] = s2;
-    foo[2] = s3;
-    (s1)++;
-    cout << s1 << endl;
+    // int x = 7;
+    // char *p = reinterpret_cast<char *>(&x);
 
-    int *p = new int;
+    // cout << &p << endl;
 
-    delete p;
-
-    int *q = new int[5];
-
-    delete[] q;
     return 0;
 }
+
+// Main GRAVEYARD
+
+// int main()
+// {
+//     char *foo[2];
+//     char s1[] = "Peter", s2[] = "Justin", s3[] = "Ramos";
+
+//     foo[0] = s1;
+//     foo[1] = s2;
+//     foo[2] = s3;
+//     // (s1)++;
+//     cout << s1 << endl;
+
+//     int *p = new int;
+
+//     delete p;
+
+//     int *q = new int[5];
+
+//     delete[] q;
+//     return 0;
+// }
